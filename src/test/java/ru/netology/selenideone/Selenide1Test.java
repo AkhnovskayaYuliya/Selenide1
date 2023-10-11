c2c42166c8593805fa02ae4e69829ed26a5e5194
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-10-11 16:19:02 +0300

    Add change in package

--- src/test/java/ru/netology/selenideone/Selenide1Test.java
+++ src/test/java/ru/netology/selenideone/Selenide1Test.java
@@ -1,7 +1,6 @@
-package ru.netology.Selenide1;
+package ru.netology.selenideone;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,7 +8,8 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class Selenide1Test {
     private String generateDate(int addDays, String pattern) {
@@ -33,5 +33,6 @@ public class Selenide1Test {
 
 
     }
-
 }
+
+
